68bd3f05887f67303209ce85a3c8e6f9194a9381
daking0 <angk226@gmail.com>
2022-12-22 17:58:21 +0900

    55강 진행중

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,15 +1,25 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
     @GetMapping({"","/"})
-    public String index(){
-        return "index";
+    public String index(Model model,@PageableDefault(size = 3,sort = "id",direction = Sort.Direction.DESC) Pageable pageable){
+        model.addAttribute("boards",boardService.글목록(pageable));
+        return "index"; //viewResolver 작동
     }
 
     //USER권한이 필요
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -4,8 +4,13 @@ import com.cos.blog.model.Board;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
 
 @Service
 public class BoardService {
@@ -20,4 +25,7 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    public Page<Board> 글목록(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
 }

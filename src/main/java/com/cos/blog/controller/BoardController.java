7b48bd7bbd3790de83576f3e5cc9f11a567ad8e0
daking0 <angk226@gmail.com>
2022-12-26 13:05:35 +0900

    58강까지 완료

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -22,6 +24,17 @@ public class BoardController {
         return "index"; //viewResolver 작동
     }
 
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id,Model model){
+        model.addAttribute("board",boardService.글상세보기(id));
+        return "board/detail";
+    }
+
+    @GetMapping("board/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model){
+        model.addAttribute("board",boardService.글상세보기(id));
+        return "board/updateForm";
+    }
     //USER권한이 필요
     @GetMapping("/board/saveForm")
     public String saveForm(){
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
-
 
 @Service
 public class BoardService {
@@ -25,7 +23,33 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    @Transactional(readOnly = true)
     public Page<Board> 글목록(Pageable pageable) {
         return boardRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Board 글상세보기(int id) {
+        return boardRepository.findById(id)
+                .orElseThrow(()->{
+                    return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
+                });
+    }
+
+    @Transactional
+    public void 글삭제하기(int id) {
+        boardRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void 글수정하기(int id, Board requestBoard) {
+        // 영속화를 먼저 시킨다
+        Board board = boardRepository.findById(id)
+                .orElseThrow(()->{
+                    return new IllegalArgumentException("글 찾기 실패: 아이디를 찾을 수 없습니다.");
+                }); //영속화 완료
+        board.setTitle(requestBoard.getTitle());
+        board.setContent(requestBoard.getContent());
+        //해당 함수로 종료시(Service가 종료될 때) 트랜잭션이 종료된다. 이때 더티체킹이 일어나서 자동업데이트가 된다(db flush)
     }
 }
